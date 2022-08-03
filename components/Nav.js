5be5ff9bf3b0e5c435e6e7ccea6af2c0029f20f6
maxwellyoung <maxtheyoung@gmail.com>
2022-08-04 02:34:06 +1200

    added projects

--- components/Nav.js
+++ components/Nav.js
@@ -5,18 +5,27 @@ import React from 'react'
 
 const Nav = () => {
 	return (
-		<nav className={navStyles.nav}>
-			<ul>
-				<li>
-					<Link href="/">Home</Link>
-				</li>
-				<li>
-					<Link href="/projects">Projects</Link>
-				</li>
-				<li>
-					<Link href="/resume.pdf">Resume</Link>
-				</li>
-			</ul>
+		<nav className="navContainer">
+			<div>
+				<nav className={navStyles.nav}>
+					<ul>
+						<li>
+							<Link href="/">Home</Link>
+						</li>
+						<li>
+							<Link href="/projects">Projects</Link>
+						</li>
+						<li>
+							<Link href="/resume.pdf">Resume</Link>
+						</li>
+					</ul>
+				</nav>
+				<div className="menu" id="menu">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
 		</nav>
 	)
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,27 +1,66 @@
 import React from 'react'
+import Image from 'next/image'
+import maplearnPic from '../public/images/maplearn1.png'
+import imdbPic from '../public/images/imdb.png'
+import favMoviesPic from '../public/images/favmovies.png'
+import madlibPic from '../public/images/madlib.png'
 
 const projects = () => {
 	return (
-		<>
-			<div>
+		<div className="columns">
+			<div className="column">
 				<h3>
 					<a href="https://maplearn.herokuapp.com/">Map Learn</a>
 				</h3>
-				<p></p>
+				<a href="https://maplearn.herokuapp.com/">
+					<Image
+						src={maplearnPic}
+						alt="Picture of the Map Learn website"
+						// width={800}
+						// height={500}
+						layout="responsive"
+					/>
+				</a>
 			</div>
-			<div>
+			<div className="column">
 				<h3>
 					<a href="https://maxwell-imdb.herokuapp.com/">iMDB Api</a>
 				</h3>
-				<p></p>
+				<a href="https://maxwell-imdb.herokuapp.com/">
+					<Image
+						src={imdbPic}
+						alt="Picture of the iMDB website"
+						layout="responsive"
+					/>
+				</a>
 			</div>
-			<div>
+			<div className="column">
 				<h3>
-					<a href="">Favourite Movies</a>
+					<a href="https://maxwells-favourite-movies.herokuapp.com/">
+						Favourite Movies
+					</a>
 				</h3>
-				<p></p>
+				<a href="https://maxwells-favourite-movies.herokuapp.com/">
+					<Image
+						src={favMoviesPic}
+						alt="Picture of the favourite movies website"
+						layout="responsive"
+					/>
+				</a>
 			</div>
-		</>
+			<div className="column">
+				<h3>
+					<a href="https://friday-madlib.herokuapp.com/">Mad Libs</a>
+				</h3>
+				<a href="https://friday-madlib.herokuapp.com/">
+					<Image
+						src={madlibPic}
+						alt="Picture of Madlib website"
+						layout="responsive"
+					/>
+				</a>
+			</div>
+		</div>
 	)
 }
 
